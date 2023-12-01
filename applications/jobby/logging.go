58d7a7da8c0f568b299f99dcdf6e8fbe2a33f3a8
Vernon Wells <v.wells@f5.com>
2023-11-30 16:48:32 -0800

    Empty runner

--- applications/jobby/logging.go
+++ applications/jobby/logging.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+
+	"github.com/blorticus-go/jobber"
 )
 
 type Logger struct {
-	fatalMessageDestination  io.Writer
-	normalMessageDestination io.Writer
+	fatalMessageDestination          io.Writer
+	normalMessageDestination         io.Writer
+	contextFieldWidthPrintfSpecifier string
 }
 
 func NewLogger() *Logger {
 	return &Logger{
-		fatalMessageDestination:  os.Stderr,
-		normalMessageDestination: os.Stdout,
+		fatalMessageDestination:          os.Stderr,
+		normalMessageDestination:         os.Stdout,
+		contextFieldWidthPrintfSpecifier: "%s",
 	}
 }
 
 func (l *Logger) fatalFinalEvent() {
 	os.Exit(1)
+}
+
+func (l *Logger) SetContextFieldWidth(maximumUnitNameLength uint, maximumCaseNameLength uint) *Logger {
+	fieldWidth := maximumUnitNameLength + maximumCaseNameLength + 3 // +1 for delimiter and brackets
+	l.contextFieldWidthPrintfSpecifier = fmt.Sprintf("%%-%d.%ds", fieldWidth, fieldWidth)
+	return l
 }
 
 func (l *Logger) Fatalf(formatString string, a ...any) {
@@ -39,4 +50,54 @@ func (l *Logger) DieIfError(err error, formatStringThenSprintfArgs ...any) {
 
 func (l *Logger) Say(formatString string, a ...any) {
 	fmt.Fprintf(l.normalMessageDestination, formatString+"\n", a...)
+}
+
+func (l *Logger) SayContextually(context *jobber.EventContext, formatString string, a ...any) {
+	f := fmt.Sprintf("%s %s", l.possiblyFixedWidthContextString(context), formatString)
+	l.Say(f, a...)
+}
+
+func (l *Logger) possiblyFixedWidthContextString(context *jobber.EventContext) string {
+	if context.CaseName == "" {
+		r := fmt.Sprintf("[%s]", context.UnitName)
+		return fmt.Sprintf(l.contextFieldWidthPrintfSpecifier, r)
+	}
+
+	r := fmt.Sprintf("[%s/%s]", context.UnitName, context.CaseName)
+	return fmt.Sprintf(l.contextFieldWidthPrintfSpecifier, r)
+}
+
+func (l *Logger) LogEventMessage(event *jobber.Event) {
+	switch event.Type {
+	case jobber.ResourceCreationSuccess:
+		l.SayContextually(&event.Context, "Resource (%s) created successfully", justTheTargetFrom(event.PipelinePathId))
+	case jobber.ResourceCreatingFailure:
+		l.SayContextually(&event.Context, "Resource (%s) creation failed: %s", justTheTargetFrom(event.PipelinePathId), event.ResourceInformation.Error)
+	case jobber.ResourceDeletionSuccess:
+		l.SayContextually(&event.Context, "Resource (%s) deleted successfully", justTheTargetFrom(event.PipelinePathId))
+	case jobber.ResourceDeletionFailure:
+		l.SayContextually(&event.Context, "Resource (%s) deletion failed: %s", justTheTargetFrom(event.PipelinePathId), event.ResourceInformation.Error)
+	case jobber.ValuesTransformSuccess:
+		l.SayContextually(&event.Context, "ValueTransform (%s) completed successfully", justTheTargetFrom(event.PipelinePathId))
+	case jobber.ValuesTransformFailure:
+		l.SayContextually(&event.Context, "ValueTransform (%s) failed: %s", justTheTargetFrom(event.PipelinePathId), event.ValuesTransformInformation.Error)
+	case jobber.ExecutableRunSuccess:
+		l.SayContextually(&event.Context, "Executable (%s) ran successfully", justTheTargetFrom(event.PipelinePathId))
+	case jobber.ExecutableRunFailure:
+		l.SayContextually(&event.Context, "Executable (%s) run failed: %s", justTheTargetFrom(event.PipelinePathId), event.ExecuableInformation.Error)
+	case jobber.TestUnitStarted:
+		l.SayContextually(&event.Context, "Unit started")
+	case jobber.TestUnitCompletedSuccessfully:
+		l.SayContextually(&event.Context, "Unit completed succesfully")
+	case jobber.TestCaseStarted:
+		l.SayContextually(&event.Context, "Test case started")
+	case jobber.TestCaseCompletedSuccessfully:
+		l.SayContextually(&event.Context, "Test case completed succesfully")
+	case jobber.TestingCompletedSuccesfully:
+		l.SayContextually(&event.Context, "Testing completed successfully")
+	}
+}
+
+func justTheTargetFrom(typeAndTarget string) string {
+	return (strings.Split(typeAndTarget, "/"))[1]
 }
--- applications/jobby/main.go
+++ applications/jobby/main.go
@@ -15,8 +15,23 @@ func main() {
 	client, err := jobber.NewClientUsingKubeconfigFile(kubeconfigFilePath)
 	logger.DieIfError(err, "failed to process kubeconfig")
 
-	runner := jobber.NewRunner(client)
-
-	err = runner.ReadConfigurationFromFile(clargs.ConfigurationFilePath)
+	config, err := jobber.ReadConfigurationYamlFromFile(clargs.ConfigurationFilePath)
 	logger.DieIfError(err)
+
+	logger.SetContextFieldWidth(config.CharactersInLongestUnitName(), config.CharactersInLongestCaseName())
+
+	runner := jobber.NewRunner(config, client)
+
+	eventChannel := make(chan *jobber.Event)
+
+	go runner.RunTest(eventChannel)
+
+	for {
+		event := <-eventChannel
+		logger.LogEventMessage(event)
+
+		if event.Type == jobber.TestingCompletedSuccesfully || event.ForcedTestToAbortOnError {
+			break
+		}
+	}
 }
